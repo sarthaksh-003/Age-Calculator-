a1d8ad27f4968975646073ced50bb6a320a6deef
Sarthak Kshirsagar <73735622+sarthaksh-003@users.noreply.github.com>
2025-07-01 21:05:52 +0530

    Add files via upload

---
+++ Age Calculator.py
@@ -0,0 +1,60 @@
+from tkinter import *
+from datetime import date
+
+root = Tk()
+root.geometry('280x300')
+root.resizable(0, 0)
+root.title('Age Calculator')
+statement = Label(root)
+
+
+
+
+def ageCalc():
+    global statement
+    statement.destroy()
+    today = date.today()
+    birthDate = date(int(yearEntry.get()), int(
+        monthEntry.get()), int(dayEntry.get()))
+    age = today.year - birthDate.year
+    if today.month < birthDate.month or today.month == birthDate.month and today.day < birthDate.day:
+        age -= 1
+    statement = Label(text=f"{nameValue.get()}'s age is {age}.")
+    statement.grid(row=6, column=1, pady=15)
+
+
+
+l1 = Label(text="Name: ")
+l1.grid(row=1, column=0)
+nameValue = StringVar()
+
+
+nameEntry = Entry(root, textvariable=nameValue)
+nameEntry.grid(row=1, column=1, padx=10, pady=10)
+
+l2 = Label(text="Year: ")
+l2.grid(row=2, column=0)
+yearValue = StringVar()
+yearEntry = Entry(root, textvariable=yearValue)
+yearEntry.grid(row=2, column=1, padx=10, pady=10)
+
+
+l3 = Label(text="Month: ")
+l3.grid(row=3, column=0)
+monthValue = StringVar()
+monthEntry = Entry(root, textvariable=monthValue)
+monthEntry.grid(row=3, column=1, padx=10, pady=10)
+
+
+l4 = Label(text="Day: ")
+l4.grid(row=4, column=0)
+dayValue = StringVar()
+dayEntry = Entry(root, textvariable=dayValue)
+dayEntry.grid(row=4, column=1, padx=10, pady=10)
+
+
+button = Button(text="Calculate age", command=ageCalc)
+button.grid(row=5, column=1)
+
+
+root.mainloop()
